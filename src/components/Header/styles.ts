e41b20f7dc612ce4a460df2dd1cdf3a06390800a
Anderson <anderalw@gmail.com>
2020-04-24 02:41:47 -0300

    update

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,13 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 
 interface ContainerProps {
   size?: 'small' | 'large';
 }
 
+interface StyledLinkProps {
+  currentpage: boolean;
+}
+
 export const Container = styled.div<ContainerProps>`
   background: #5636d3;
   padding: 30px 0;
-
   header {
     width: 1120px;
     margin: 0 auto;
@@ -15,22 +19,28 @@ export const Container = styled.div<ContainerProps>`
     display: flex;
     align-items: center;
     justify-content: space-between;
-
     nav {
       a {
         color: #fff;
         text-decoration: none;
         font-size: 16px;
         transition: opacity 0.2s;
-
+        padding-bottom: 15px;
         & + a {
           margin-left: 32px;
         }
-
         &:hover {
           opacity: 0.6;
         }
       }
     }
   }
+`;
+
+export const StyledLink = styled(Link)<StyledLinkProps>`
+  ${props =>
+    props.currentpage &&
+    css`
+      border-bottom: 3px solid red;
+    `}
 `;
